2e78779c068d9d6a85e254612e9a2ea99dee87a7
ArkLuke <lucas.kenchian@hotmail.com>
2017-04-22 10:20:35 -0300

    Add files via upload

--- ep2a.py
+++ ep2a.py
@@ -1,24 +1,26 @@
 import time
 import random
 Insperdex=dict()
-Insperdex={"Pikachu":{"poder":20,"vida":100,"defesa":7,"xp1":0,"xp2":25},	
-		   "Bulbasaur":{"poder":25,"vida":100,"defesa":8,"xp1":0,"xp2":20},
-		   "Venosaur":{"poder":30,"vida":120,"defesa":10,"xp1":0,"xp2":30},
-		   "Charmander":{"poder":25,"vida":100,"defesa":8,"xp1":0,"xp2":20},
-		   "Charizard":{"poder":30,"vida":120,"defesa":10,"xp1":0,"xp2":30},
-		   "Squirtle":{"poder":25,"vida":100,"defesa":8,"xp1":0,"xp2":20},
-		   "Blastoise":{"poder":30,"vida":120,"defesa":10,"xp1":0,"xp2":30},
-		   "Abra":{"poder":20,"vida":80,"defesa":5,"xp1":9,"xp2":15},
-		   "Tentacool":{"poder":15,"vida":60,"defesa":5,"xp1":0,"xp2":10},
-		   "Geodude":{"poder":20,"vida":50,"defesa":6,"xp1":0,"xp2":15},
-		   "Onix":{"poder":25,"vida":100,"defesa":9,"xp1":0,"xp2":25},
-		   "Horsea":{"poder":15,"vida":60,"defesa":5,"xp1":0,"xp2":15},
-		   "Zubat":{"poder":15,"vida":50,"defesa":5,"xp1":0,"xp2":10}}
+Insperdex={"Pikachu":{"poder":20,"vida":100,"defesa":7,"xpatual":0,"xpevolucao":1,"xp2":25},	
+		   "Bulbasaur":{"poder":25,"vida":100,"defesa":8,"xpatual":0,"xpevolucao":1,"xp2":20},
+		   "Squirtle":{"poder":25,"vida":100,"defesa":8,"xpatual":0,"xpevolucao":1,"xp2":20},
+		   "Charmander":{"poder":25,"vida":100,"defesa":8,"xpatual":0,"xpevolucao":1,"xp2":20},
+		   "Ivysaur":{"poder":30,"vida":110,"defesa":9,"xpatual":0,"xpevolucao":1,"xp2":20},
+		   "Wartortle":{"poder":30,"vida":110,"defesa":9,"xpatual":0,"xpevolucao":1,"xp2":20},
+		   "Charmeleon":{"poder":30,"vida":110,"defesa":9,"xpatual":0,"xpevolucao":1,"xp2":20},
+		   "Venusaur":{"poder":35,"vida":120,"defesa":10,"xpatual":0,"xpevolucao":1,"xp2":30},
+		   "Blastoise":{"poder":35,"vida":120,"defesa":10,"xpatual":0,"xpevolucao":1,"xp2":30},
+		   "Charizard":{"poder":35,"vida":120,"defesa":10,"xpatual":0,"xpevolucao":1,"xp2":30},
+		   "Abra":{"poder":20,"vida":80,"defesa":5,"xpatual":0,"xpevolucao":1,"xp2":15},
+		   "Tentacool":{"poder":15,"vida":60,"defesa":5,"xpatual":0,"xpevolucao":1,"xp2":10},
+		   "Geodude":{"poder":20,"vida":70,"defesa":6,"xpatual":0,"xpevolucao":1,"xp2":15},
+		   "Onix":{"poder":15,"vida":80,"defesa":9,"xpatual":0,"xpevolucao":1,"xp2":25},
+		   "Horsea":{"poder":15,"vida":60,"defesa":5,"xpatual":0,"xpevolucao":1,"xp2":15},
+		   "Zubat":{"poder":15,"vida":50,"defesa":5,"xpatual":0,"xpevolucao":1,"xp2":10}}
 		   
 Computador=[]
 
 probbilidadefuga1 = [0,1,2,3,4,5,6,7,8,9,10]
-probbilidadefuga2 = [0,1,2,3,4,5,6,7,8,9,10]
 
 #FUNÇÃO RETORNA UM NÚMERO ALEATÓRIO NUMA DISTRIBUIÇÃO NORMAL CENTRADA EM NUM
 def distnormal(num):
@@ -43,7 +45,13 @@ def ataca(atacante,defensor):
 			Insperdex[defensor]["vida"]=Insperdex[defensor]["vida"]-((ataque-defesa))
 			if Insperdex[defensor]["vida"] < 0:
 				Insperdex[defensor]["vida"] = 0	
-		   
+
+def evolve(inspermon):
+	if 
+
+
+
+#MAIN CODE		   
 print("Bem vindo ao Inspermon")
 time.sleep(0.5)
 
@@ -60,10 +68,9 @@ while True:
 	if usuario == "3":
 		usuario = "Squirtle"
 	if usuario == "Charmander" or usuario == "Bulbasaur" or usuario == "Squirtle":
-		Computador.append(Insperdex[usuario])
+		Computador.append(usuario)
 		print("Você escolheu o {}".format(usuario))
 		break
-		
 	else:
 		print("Escolha um Inspermon válido")
 		
@@ -72,7 +79,6 @@ while True:
 while True:
 	x=random.choice(list(Insperdex.keys()))
 	y=random.choice(list(probbilidadefuga1))
-	z=random.choice(list(probbilidadefuga2))
 	a = input("Você deseja passear, dormir ou ver seu computador?")
 	if a == "dormir":
 		print("Você encerrou o jogo, salvando...")
@@ -85,7 +91,8 @@ while True:
 		print("Passeando...")
 		time.sleep(1)
 		print("Aaah...Você encontrou um {} selvagem.".format(x))
-		Computador.append(Insperdex[x])
+		Computador.append(x)
+
 				
 	#INICIO BATALHA	
 		b= input("Deseja batalhar ou fugir?")
@@ -95,9 +102,9 @@ while True:
 					
 			if y >= 5:
 				print("Fuga mal sucedida!")
-				b=="batalhar"
+				b="batalhar"
 				
-		elif b == "batalhar":
+		if b == "batalhar":
 			print("Iniciando batalha...")
 			time.sleep(1)
 			print("Inspermon : {}".format(x))
@@ -106,12 +113,11 @@ while True:
 			print("defesa = {}".format(Insperdex[x]["defesa"]))
 			c= input("Voce deseja atacar ou fugir?")
 			if c == "fugir":
-				if z < 5:
+				if y < 5:
 					print("Você fugiu da batalha com sucesso")
 			
-				else:
-					if z >= 5:
-						print("Fuga mal sucedida, VOCÊ TERÁ que batalhar!")
+				if y >= 5:
+					print("Fuga mal sucedida, VOCÊ TERÁ que batalhar!")
 				
 			elif c == "atacar":
 				while Insperdex[x]["vida"]>0 and Insperdex[usuario]["vida"]>0:
@@ -130,21 +136,13 @@ while True:
 					Insperdex[usuario]["vida"]=Insperdex[usuario]["vida"]-(Insperdex[x]["poder"]-Insperdex[usuario]["defesa"])
 					if Insperdex[usuario]["vida"]<=0:
 						print("Voce perdeu a batalha")
-					
-"""def funcaoxp
-	xp = Insperdex[usuario]["xp1"]+Insperdex[x]["xp2"]
-	return xp
-	xp=xp+Insperdex[x]["xp2"]
-	return xp
+						time.sleep(1)
+						print("Retornando ao InsperCenter para recuperar seu Inspermon...")
+						time.sleep(1)
+						print("A vida do seu Inspermon foi restaurada!")
 
-def funcaoevoluir
-	if usuario == Charmander
-	return Charizard
-	if usuario == Squirtle
-	return Blastoise
-	if usuario == Bulbasaur
-	return Venosaur}
-COISAS QUE FALTAM:
+
+"""COISAS QUE FALTAM:
 → FUNCIONALIDADE 1: FEITA
 → FUNCIONALIDADE 2: FEITA
 → FUNCIONALIDADE 3: FEITA
